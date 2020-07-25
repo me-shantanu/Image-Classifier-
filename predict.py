367146c116a8c465bd4ece88b4ce7450a23b92ac
Shantanu Mishra <shantmiitanphy@gmail.com>
2020-07-25 09:58:10 +0530

    Finial commit

--- predict.py
+++ predict.py
@@ -1,29 +1,41 @@
-import tensorflow as tf
+import tensorflow as tf 
+import tensorflow_hub as hub 
 from PIL import Image
-import tensorflow_hub as hub
-from utils import process_image, get_class_names, load_model
+import json
 
 import argparse
 import numpy as np
-import json
+
+# make all nessosory functions which we are going to use in our projects
+def get_name(json_File):
+    with open(json_File, 'r') as f:
+        class_names =json.load(f)
+    my_class = {}
+    for i in class_names:
+        my_class[str(int(i)-1)] = class_names[i]
+    return my_class
+
+def load(m_path):
+    model = tf.keras.models.load_model(m_path, custom_objects={'KerasLayer':hub.KerasLayer})
+    print(model.summary())
+    return model
+
+def pre_process_image(np_image):
+    tf_image = tf.image.convert_image_dtype(np_image, dtype=tf.int16, saturate=False)
+    resize_imgage = tf.image.resize(tf_image,(30,30)).numpy()
+    img = resize_imgage/255
+    return img
+
 
 def predict(image_path, model_path, top_k, all_class_names):
     top_k = int(top_k)
-    print(top_k, type(top_k))
-    model = load_model(model_path)
+    model = load(model_path)
 
     img = Image.open(image_path)
     test_image = np.asarray(img)
-
-    # processing the image
-    processed_test_image = process_image(test_image)
-    
-    # fetching prediction probabilities
+    processed_test_image = pre_process_image(test_image)
     prob_preds = model.predict(np.expand_dims(processed_test_image,axis=0))
     prob_preds = prob_preds[0].tolist()
-    
-    
-    #top 1 prediction
     top_pred_class_id = model.predict_classes(np.expand_dims(processed_test_image,axis=0))
     top_pred_class_prob = prob_preds[top_pred_class_id[0]]
     pred_class = all_class_names[str(top_pred_class_id[0])]
@@ -41,7 +53,7 @@ def predict(image_path, model_path, top_k, all_class_names):
     class_prob_dict = dict(zip(class_labels, probs_topk))       
     print("\nTop K classes along with associated probabilities :\n",class_prob_dict)
 
-#     return probs_topk, classes_topk, top_pred_class_id, top_pred_class_prob
+
 
 
 if __name__ == "__main__":
@@ -52,9 +64,7 @@ if __name__ == "__main__":
     parser.add_argument("--top_k", help="Fetch top k predictions", required = False, default = 3)
     parser.add_argument("--category_names", help="Class map json file", required = False, default = "label_map.json")
     args = parser.parse_args()
-
-    all_class_names = get_class_names(args.category_names)
-#     print("Displaying class names:\n",all_class_names)
-
+    all_class_names = get_name(args.category_names)
     predict(args.image_path, args.saved_model, args.top_k, all_class_names)
-#     probs, classes, top_class_id, top_class_prob = predict(args.image_path, args.saved_model, args.top_k, all_class_names)
+
+
